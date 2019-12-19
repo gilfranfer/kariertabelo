bfc238c5f22e83f452fff5ea8b0385eb3b1940d0
Fernando Gil <gil.franfer@gmail.com>
2019-12-19 17:34:45 -0600

    ng route module setup

--- resources/js/kariertabelo-site.js
+++ resources/js/kariertabelo-site.js
@@ -1,2 +1,15 @@
-let app = angular.module('Kariertabelo', []);
+let app = angular.module('Kariertabelo', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/home', {
+    templateUrl: 'views/landing.html',
+    controller: 'KariertabeloCtrl'
+  })
+  .otherwise({
+    redirectTo: '/home'
+  });
+  $locationProvider.html5Mode(false);
+});
+
 app.controller('KariertabeloCtrl', function($scope) {});
