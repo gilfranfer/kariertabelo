1c1f64b3ff8c9247d9594db46f3e1751b154d71c
Fernando Gil <gil.franfer@gmail.com>
2019-12-21 11:43:48 -0600

    Resume Example update

--- resources/js/kariertabelo-site.js
+++ resources/js/kariertabelo-site.js
@@ -151,64 +151,64 @@ app.factory('SignUpSvc', function($firebaseAuth){
 app.controller('ExampleResumeCtrl', function($scope) {
 
   $scope.updateResumeColor = function(color){
-    $scope.colors.profile.column = color;
-    $scope.colors.profile.profile = color;
-    $scope.colors.profile.bio = color;
-    $scope.colors.profile.contact = color;
-    $scope.colors.profile.education = color;
-    $scope.colors.profile.languages = color;
-    $scope.colors.profile.interests = color;
+    $scope.config.colors.profile.column = color;
+    $scope.config.colors.profile.profile = color;
+    $scope.config.colors.profile.bio = color;
+    $scope.config.colors.profile.contact = color;
+    $scope.config.colors.profile.education = color;
+    $scope.config.colors.profile.languages = color;
+    $scope.config.colors.profile.interests = color;
 
-    $scope.colors.profile.interests = color;
-    $scope.colors.resume.title = color;
-    $scope.colors.resume.circle = color;
-    $scope.colors.resume.link = color;
-    $scope.colors.resume.bar = color;
-    $scope.colors.resume.skillBadge = color;
+    $scope.config.colors.resume.title = color;
+    $scope.config.colors.resume.circle = color;
+    $scope.config.colors.resume.link = color;
+    $scope.config.colors.resume.bar = color;
+    $scope.config.colors.resume.skillBadge = color;
   };
 
   $scope.setProfileColSide = function(side){
       if(side == "left"){
-        $scope.order = {
+        $scope.config.order = {
           profile:1, resume:2
         };
       }else{
-        $scope.order = {
+        $scope.config.order = {
           profile:2, resume:1
         };
       }
   };
 
-  $scope.order = {
-    profile:1,
-    resume:2
-  };
-
-  $scope.colors = {
-    profile:{
-      column:"#28a745",
-      profile:"#28a745",
-      bio:"#28a745",
-      contact:"#28a745",
-      education:"#28a745",
-      languages:"#28a745",
-      interests:"#28a745",
-      text:"#fff",
-      link:"#ddd"
+  $scope.config = {
+    order: {
+      profile:1,
+      resume:2
     },
-    resume:{
-      title:"#28a745",
-      circle:"#28a745",
-      icon:"#fff",
-      text:"#444",
-      link:"#28a745",
-      bar:"#28a745",
-      skillText:"#fff",
-      skillBadge:"#28a745"
+    colors:{
+      profile:{
+        column:"#28a745",
+        profile:"#28a745",
+        bio:"#28a745",
+        contact:"#28a745",
+        education:"#28a745",
+        languages:"#28a745",
+        interests:"#28a745",
+        text:"#fff",
+        link:"#ddd"
+      },
+      resume:{
+        title:"#28a745",
+        circle:"#28a745",
+        icon:"#fff",
+        text:"#444",
+        link:"#28a745",
+        bar:"#28a745",
+        skillText:"#fff",
+        skillBadge:"#28a745"
+      }
     }
   };
 
-  $scope.titles = {
+  $scope.labels = {
     education:"Educación",
     languages:"Idiomas",
     interests:"Intereses",
@@ -218,14 +218,14 @@ app.controller('ExampleResumeCtrl', function($scope) {
     skills:"Habilidades y Conocimientos"
   };
 
-  /* recordId is used to order the entries. Order is from larger to smaller number, and IDs must not be repeated */
-  $scope.data = {
+  $scope.resumeData = {
+    summary:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     profile:{
-      name:"Francisco Gil",
+      firstname:"Francisco", lastname:"Gil",
       position:"Project Manager",
-      location:"Veracruz, México"
+      location:"Veracruz, México",
+      bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
-    bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     contact:{
       email:"gil.franfer@gmail.com",
       phone:"+52 2211000011",
@@ -238,50 +238,45 @@ app.controller('ExampleResumeCtrl', function($scope) {
       /*,whatsapp:"",snapchat:""*/
     },
     education:[
-      {recordId:1, degree:"Ingeniería de Software", school:"Universidad", period:"2008 - 2012"},
-      {recordId:2, degree:"Administración de Software", school:"Universidad", period:"2017 - 2019"},
+      {order:1, degree:"Ingeniería de Software", school:"Universidad", period:"2008 - 2012"},
+      {order:2, degree:"Administración de Software", school:"Universidad", period:"2017 - 2019"},
     ],
     languages:[
-      {recordId:1, language:"Español", level:"Nativo"},
-      {recordId:2, language:"Inglés", level:"Avanzado"},
-      {recordId:3, language:"Alemán", level:"Básico"},
-      {recordId:4, language:"Portugués", level:"Básico"}
+      {order:1, language:"Español", level:"Nativo"},
+      {order:2, language:"Inglés", level:"Avanzado"},
+      {order:3, language:"Alemán", level:"Básico"},
+      {order:4, language:"Portugués", level:"Básico"}
     ],
     interests:[
-      {recordId:1, interest:"Fotografía"},
-      {recordId:2, interest:"Programación"},
-      {recordId:3, interest:"Viajes"}
+      {order:1, interest:"Fotografía"},
+      {order:2, interest:"Programación"},
+      {order:3, interest:"Viajes"}
     ],
-    resume:{
-      summary:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      work:[
-        {recordId:1, role:"Desarrollador de Software", employer:"Softtek", location:"Monterrey, México", period:"Agosto 2011 - Junio 2014", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-        {recordId:2, role:"Líder Técnico", employer:"TCS México", location:"Guadalajara, México", period:"Junio 2014 - Marzo 2017", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-        {recordId:3, role:"Project Manager", employer:"TCS America", location:"San Antonio, Texas", period:"Abril 2017 - Presente", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-      ],
-      projects:[
-        {recordId:1, name:"Okulus", url:"http://okulus.netlify.com", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-        {recordId:2, name:"Kariertabelo", url:"http://kariertabelo.netlify.com", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-      ],
-      skillsBar:[
-        {recordId:1, skill:"Python", level:70},
-        {recordId:2, skill:"Kotlin", level:50},
-        {recordId:3, skill:"JavaScript", level:75},
-        {recordId:5, skill:"Java", level:90}
-      ],
-      skillsTag:[
-        {recordId:1, skill:"Angular"},
-        {recordId:2, skill:"Firebase"},
-        {recordId:3, skill:"Bootstrap"},
-        {recordId:4, skill:"React"},
-        {recordId:5, skill:"Flex"},
-        {recordId:6, skill:"Foundation"},
-        {recordId:7, skill:"Oracle"},
-        {recordId:8, skill:"Couch DB"},
-        {recordId:9, skill:"Kafka"},
-        {recordId:10, skill:"Splunk"}
-      ]
-    }
+    work:[
+      {order:1, role:"Desarrollador de Software", employer:"Softtek", location:"Monterrey, México", period:"Agosto 2011 - Junio 2014", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+      {order:2, role:"Líder Técnico", employer:"TCS México", location:"Guadalajara, México", period:"Junio 2014 - Marzo 2017", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+      {order:3, role:"Project Manager", employer:"TCS America", location:"San Antonio, Texas", period:"Abril 2017 - Presente", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+    ],
+    projects:[
+      {order:1, name:"Okulus", url:"http://okulus.netlify.com", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+      {order:2, name:"Kariertabelo", url:"http://kariertabelo.netlify.com", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+    ],
+    skills:[
+      {order:1, skill:"Angular"},
+      {order:2, skill:"Firebase"},
+      {order:3, skill:"Bootstrap"},
+      {order:4, skill:"React"},
+      {order:5, skill:"Flex"},
+      {order:6, skill:"Foundation"},
+      {order:7, skill:"Oracle"},
+      {order:8, skill:"Couch DB"},
+      {order:9, skill:"Kafka"},
+      {order:10, skill:"Splunk"},
+      {order:11, skill:"Python", level:70},
+      {order:12, skill:"Kotlin", level:50},
+      {order:13, skill:"JavaScript", level:75},
+      {order:14, skill:"Java", level:90}
+    ]
   };
 
 });
